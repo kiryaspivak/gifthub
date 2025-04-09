b15ee13bfa32a012c15ee7cc201ac80dadf73845
kiryaspivak <kiryaspivak@gmail.com>
2025-04-09 23:32:35 +0300

    Добавлен интерфейс для логики взаимодействия с пользователями

---
+++ backend/src/Gifthub.Domain/Interfaces/IUsersService.cs
@@ -0,0 +1,12 @@
+﻿using Gifthub.Domain.Entities;
+
+namespace Gifthub.Domain.Interfaces;
+
+public interface IUsersService
+{
+    Task<User?> GetUsersAsync();
+    Task<User?> GetUserByIdAsync(Guid id);
+    Task<User?> GetUserByNameAsync(string name);
+    Task<User?> GetUserByEmailAsync(string email);
+    Task<User?> UpdateUserAsync(User user, string newName);
+} 
